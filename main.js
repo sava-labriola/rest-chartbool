b2534db48556ea9f554787cd89e2a33ec90c8dcb
sava-labriola <labriolasava00@gmail.com>
2020-06-10 18:30:59 +0200

    Milestone 1

--- main.js
+++ main.js
@@ -7,8 +7,11 @@ function chiamata_ajax() {
         'url': "http://157.230.17.132:4011/sales",
         'method': 'GET',
         'success': function(data) {
+            console.log(data);
             vendite_mese(data);
-            chart();
+            vendite_per_venditore(data);
+            chart1();
+            chart2();
         },
         'error': function() {
             alert('errore');
@@ -17,19 +20,30 @@ function chiamata_ajax() {
 }
 
 function vendite_mese(array) {
-var spese_mensili = {};
+var spese_mensili = {
+    'January': 0,
+    'February': 0,
+    'March': 0,
+    'April': 0,
+    'May': 0,
+    'June': 0,
+    'July': 0,
+    'August': 0,
+    'September': 0,
+    'October': 0,
+    'November': 0,
+    'December': 0
+};
     for (var i = 0; i < array.length; i++) {
         var cifra = array[i].amount;
         console.log(cifra);
         var data = array[i].date;
         console.log(data);
         var formato_data = moment(data, 'DD/MM/YYYY');
-        var mese = formato_data.format('MM');
+        var mese = formato_data.format('MMMM');
         console.log(mese);
-        if(!spese_mensili.hasOwnProperty(mese)) {
-          spese_mensili[mese] = cifra;
-        } else {
-        spese_mensili[mese] += cifra;
+        if(spese_mensili.hasOwnProperty(mese)) {
+            spese_mensili[mese] += cifra;
         }
     }
     console.log(spese_mensili);
@@ -38,7 +52,25 @@ var spese_mensili = {};
     valori = Object.values(spese_mensili);
 }
 
-function chart() {
+function vendite_per_venditore(array) {
+    var venditori = {};
+    var spese_totali = 0;
+    for (var i = 0; i < array.length; i++) {
+        spese_totali += array[i].amount;
+        var cifra = array[i].amount;
+        var venditore = array[i].salesman;
+        if(!venditori.hasOwnProperty(venditore)) {
+            venditori[venditore] = cifra;
+        } else {
+            venditori[venditore] += cifra;
+        }
+    }
+    chiavi2 = Object.keys(venditori);
+
+    valori2 = Object.values(venditori);
+}
+
+function chart1() {
     var ctx = $('#myChart')[0].getContext('2d');
 
     var myChart = new Chart(ctx, {
@@ -75,6 +107,34 @@ function chart() {
                     'rgba(255, 99, 132, 1)',
                     'rgba(255, 99, 132, 1)',
                     'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1,
+            }]
+        },
+    });
+}
+
+function chart2() {
+    var ctx = $('#myChart2')[0].getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: chiavi2,
+            datasets: [{
+                label: 'Vendite per mese',
+                data: valori2,
+                backgroundColor: [
+                    'rgb(255, 165, 0, 0.2)',
+                    'rgb(60, 179, 113, 0.2)',
+                    'rgb(255, 0, 0, 0.2)',
+                    'rgb(106, 90, 205, 0.2)',
+                ],
+                borderColor: [
+                    'rgb(255, 165, 0, 1)',
+                    'rgb(60, 179, 113, 1)',
+                    'rgb(255, 0, 0, 1)',
+                    'rgb(106, 90, 205, 1)',
                 ],
                 borderWidth: 1,
             }]
